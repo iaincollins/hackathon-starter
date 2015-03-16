1bfb3eb269696507da4325e5130d80ad4736d362
Iain Collins <me@iaincollins.com>
2015-03-16 16:43:57 +0000

    Adding missing app.js file

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+$(function() { 
+  $('a[data-toggle="modal"]').attr('href', "#");
+  $('a[data-toggle="dropdown"]').attr('href', "#");
+  $(".modal-draggable .modal-dialog").draggable({
+      handle: ".modal-header"
+  });
+});
