3c163b536001df7417f1267e47c229e7bbbe1bf9
Iain Collins <me@iaincollins.com>
2015-03-24 12:24:06 +0000

    Adding less support for CSS generation with grunt task

--- routes/theme.js
+++ routes/theme.js
@@ -3,7 +3,10 @@
  * Change the theme for the current user
  */
 exports.postTheme = function(req, res, next) {
-  if (req.body['theme-name'])
+  if (req.body['theme-name']) {
     req.session.theme = req.body['theme-name'];
+  } else {
+    req.session.theme = null; 
+  }
   res.redirect(req.session.returnTo || '/');
 };
